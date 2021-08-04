63a6421834c96fd5fab0ca129e6456636e782d5c
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2021-08-05 00:12:34 +0300

    divide for more components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
-
-const DialogItem = (props) => {
-  let path = '/dialogs/' + props.id;
-  return (
-    <div className={s.dialog + ' ' + s.active}>
-      <NavLink to={path}> {props.name} </NavLink>
-    </div>
-  )
-}
-
-const Message = (props) => {
-  return (
-    <div className={s.dialog}>{props.message}</div>
-  )
-}
+import DialogItem from './DialogItem/DialogItem.jsx';
+import Message from './Message/Message.jsx';
 
 const Dialogs = (props) => {
   let dialogs = [
