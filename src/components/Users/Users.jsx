643a763cd10545008e2c2165c65de73805e832c1
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2021-08-27 11:16:34 +0300

    remake functional component Users to class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,38 +3,38 @@ import React from 'react';
 import style from './Users.module.css';
 import userDefaultAvatar from './../../assets/img/user_avatar_default.png';
 
-const Users = (props) => {
-  let getUsers = () => {
-    if(props.users.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
         .then(response => {
-          debugger;
-          props.setUsers(response.data.items);
-        })
-    }
+          this.props.setUsers(response.data.items);
+        });
   }
-  
-  return (
-    <div>
-      <button onClick={getUsers}>Get users</button>
-      {props.users.map(user =>
-        <div key={user.id} className={style.userInfo_wrapper}>
-          <div className={style.userInfo_wrapper__1col}>
-            <img className={style.userAvatar} src={user.photos.small != null ? user.photos.small : userDefaultAvatar}
-             alt="user-avatar"/>
-            {user.followed
-              ? <button onClick={() => { props.unfollow(user.id) }}>Unfollow</button>
-              : <button onClick={() => { props.follow(user.id) }}>Follow</button>}
+
+  render() {
+    return (
+      <div>
+        {this.props.users.map(user =>
+          <div key={user.id} className={style.userInfo_wrapper}>
+            <div className={style.userInfo_wrapper__1col}>
+              <img className={style.userAvatar} src={user.photos.small != null ? user.photos.small : userDefaultAvatar}
+               alt="user-avatar"/>
+              {user.followed
+                ? <button onClick={() => { this.props.unfollow(user.id) }}>Unfollow</button>
+                : <button onClick={() => { this.props.follow(user.id) }}>Follow</button>}
+            </div>
+            <div className={style.userInfo_wrapper__2col}>
+              <div className={style.userName}>{user.name}</div>
+              <div className={style.userLocation}>Country:  <br />City: </div>
+            </div>
           </div>
-          <div className={style.userInfo_wrapper__2col}>
-            <div className={style.userName}>{user.name}</div>
-            <div className={style.userLocation}>Country:  <br />City: </div>
-          </div>
-        </div>
-      )
-      }
-    </div>
-  )
+        )
+        }
+      </div>
+    )
+  }
 }
 
 export default Users;
