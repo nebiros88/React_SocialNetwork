1eb87af87741469322e60aa1071d3964cd57df9b
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2021-09-13 12:51:38 +0300

    feat add new ability to change user status by doubleclick

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import s from './ProfileInfo.module.css'
 import Preloader from '../../common/Preloader/Preloader';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -7,13 +8,18 @@ const ProfileInfo = (props) => {
   }
 
   return (
-    <div >
+    <div className={s.profileInfo_wrapper}>
       <div className={s.profileImg}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGh5nnU_7Ug4dKSN3ufS3W66ZaOMsl6mk0rg&usqp=CAU' alt='profilePicture'></img>
       </div>
       <div className={s.descriptionBlock}>
-        <img src={props.profile.photos.large} />
-        ava + description
+        <img className={s.userAvatar} src={props.profile.photos.large} />
+        <div className={s.userInfo}>
+          <div className={s.userName}> user name</div>
+          <div className={s.userStatus}>
+            <ProfileStatus status='hello'/>
+          </div>
+        </div>
       </div>
     </div>
   )
